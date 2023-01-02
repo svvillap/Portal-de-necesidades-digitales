f5bc446fca0ff2fde1f81fed694bc36d9285ae59
JoseNunhez <nunhezmart@gmail.com>
2023-01-02 18:54:06 +0100

    index

---
+++ index.js
@@ -0,0 +1,3 @@
+const { listServices, createService, deleteService, updateService, doneService } = require('./Controladores/servicios');
+const { listComments, createComment } = require('./Controladores/comentarios');
+const { createUser, deleteUser, updateUser } = require('./Controladores/users');
