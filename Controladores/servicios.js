c25204b9a17b551f2473f4362dd2c98855a57f8e
sara <saravillazana@gmail.com>
2023-01-04 22:55:18 +0100

    validador de servicios

--- Controladores/servicios.js
+++ Controladores/servicios.js
@@ -1,93 +1,96 @@
-const { generateError } = require('../helpers');
 const { createService } = require('../db/services');
+const { newServiceSchema } = require('../validator/validadorServicios');
 
 const listServices = async (req, res, next) => {
-    // Este endpoint es para listar servicios
-    try {
-        res.send({
-         status: 'error',
-         message: 'Not implemented'
-        })
-     } catch (error) {
-         next(error)
-     }
+  // Este endpoint es para listar servicios
+  try {
+    res.send({
+      status: 'error',
+      message: 'Not implemented',
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 const newServiceController = async (req, res, next) => {
-    // Este endpoint es para crear servicios
-    try {
-        const userId = req.userId;
-        const { title, description, price, date, categoriaId, subcategoriaId } = req.body;
-        console.log(req.body);
-        console.log(userId, title, description, price, date);
-        // TODO: Implementar con Joi (validar que los campos no estén vacíos, y maximo de caracteres)
-        if(!title || !description || !price || !date) {
-            throw generateError('Faltan campos', 400);
-        }
-        const id = await createService(title, description, price, date, userId, categoriaId, subcategoriaId);
-        res.send({
-         status: 'ok',
-         message: `Servicio creado con el id: ${id}`
-        })
-     } catch (error) {
-         next(error)
-     }
+  // Este endpoint es para crear servicios
+  try {
+    const userId = req.userId;
+    const values = await newServiceSchema.validateAsync(req.body);
+    const { title, description, price, date, categoriaId, subcategoriaId } =
+      values;
+    const id = await createService(
+      title,
+      description,
+      price,
+      date,
+      userId,
+      categoriaId,
+      subcategoriaId
+    );
+    res.send({
+      status: 'ok',
+      message: `Servicio creado con el id: ${id}`,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 const deleteService = async (req, res, next) => {
-    // Este endpoint es para borrar servicios
-    try {
-        res.send({
-         status: 'error',
-         message: 'Not implemented'
-        })
-     } catch (error) {
-         next(error)
-     }
+  // Este endpoint es para borrar servicios
+  try {
+    res.send({
+      status: 'error',
+      message: 'Not implemented',
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 const updateService = async (req, res, next) => {
-    // Este endpoint es para actualizar servicios
-    try {
-        res.send({
-         status: 'error',
-         message: 'Not implemented'
-        })
-     } catch (error) {
-         next(error)
-     }
+  // Este endpoint es para actualizar servicios
+  try {
+    res.send({
+      status: 'error',
+      message: 'Not implemented',
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 const doneService = async (req, res, next) => {
-    // Este endpoint es para marcar servicios como realizados
-    try {
-        res.send({
-         status: 'error',
-         message: 'Not implemented'
-        })
-     } catch (error) {
-         next(error)
-     }
+  // Este endpoint es para marcar servicios como realizados
+  try {
+    res.send({
+      status: 'error',
+      message: 'Not implemented',
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 const listSingleService = async (req, res, next) => {
-    // Este endpoint es para listar un servicio
-    try {
-        res.send({
-         status: 'error',
-         message: 'Not implemented'
-        })
-     } catch (error) {
-         next(error)
-     }
+  // Este endpoint es para listar un servicio
+  try {
+    res.send({
+      status: 'error',
+      message: 'Not implemented',
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
-
 module.exports = {
-    listServices,
-    newServiceController,
-    deleteService,
-    updateService,
-    doneService,
-    listSingleService
+  listServices,
+  newServiceController,
+  deleteService,
+  updateService,
+  doneService,
+  listSingleService,
 };
--- db/services.js
+++ db/services.js
@@ -1,21 +1,46 @@
-const { generateError } = require("../helpers");
-const { getConnection } = require("./db");
+const { generateError } = require('../helpers');
+const { getConnection } = require('./db');
 
-const createService = async (title, description, price, date, userId, categoriaId, subcategoriaId) => {
-    let connection;
-    try {
-        connection = await getConnection();
-        const sqlQuery = `INSERT INTO SERVICIOS (TITULO, DESCRIPCION, PRECIO, FECHA_LIMITE, ID_USUARIOS, ID_CATEGORIAS, ID_SUBCATEGORIAS) VALUES (?, ?, ?, ?, ?, ?, ?)`;
-        const [result] = await connection.query(sqlQuery, [title, description, price, date, userId, categoriaId, subcategoriaId]);
-        return result.insertId;
-    } catch (error) {
-        throw generateError('No se ha podido crear el servicio', 500);
-    } finally {
-        if(connection) connection.release();
-    }
+const createService = async (
+  title,
+  description,
+  price,
+  date,
+  userId,
+  categoriaId,
+  subcategoriaId
+) => {
+  let connection;
+  try {
+    connection = await getConnection();
+    console.log([
+      title,
+      description,
+      price,
+      date,
+      userId,
+      categoriaId,
+      subcategoriaId,
+    ]);
+    const sqlQuery = `INSERT INTO SERVICIOS (TITULO, DESCRIPCION, PRECIO, FECHA_LIMITE, ID_USUARIOS, ID_CATEGORIAS, ID_SUBCATEGORIAS) VALUES (?, ?, ?, ?, ?, ?, ?)`;
+    const [result] = await connection.query(sqlQuery, [
+      title,
+      description,
+      price,
+      date,
+      userId,
+      categoriaId,
+      subcategoriaId,
+    ]);
+    return result.insertId;
+  } catch (error) {
+    console.log(error);
+    throw generateError('No se ha podido crear el servicio', 500);
+  } finally {
+    if (connection) connection.release();
+  }
 };
 
 module.exports = {
-    createService,
+  createService,
 };
-    
