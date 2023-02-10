e985247b95ebe43cad161ff0d26a72ebef3c7e20
SaraVillazana <saravillazana@gmail.com>
2023-02-10 12:04:37 +0100

    a;adir nuevas seeds usuarios, servicios

--- db/seeds.js
+++ db/seeds.js
@@ -26,6 +26,40 @@ async function seeds() {
       (5, 5, 'Consultas legales', 'Prestar asesoramiento correcto y oportuno a los ejecutivos sobre distintas cuestiones jurídicas.')
   `
   );
+
+  await connection.query(
+    `
+      INSERT INTO USUARIOS(id, nombre, email, contrasenha, nombre_usuario) VALUES
+      (1, 'Pedro Perez', 'pedrop@prueba.com', '12345', 'userpedro'), 
+      (2, 'Marta Sanchez', 'martas@prueba.com', '12345', 'usermarta'), 
+      (3, 'Mara G', 'marag@prueba.com', '12345', 'usermara'), 
+      (4, 'Jose Castro', 'joseg@prueba.com', '12345', 'userjose'), 
+      (5, 'Ana Paz', 'anap@prueba.com', '12345', 'userana')
+    `
+  );
+
+  await connection.query(
+    `
+      INSERT INTO SERVICIOS(id, titulo, id_categorias, id_subcategorias, id_usuarios) VALUES
+      (1, 'servicio de promagracion', 3, 3, 1), 
+      (2, 'servicio de traduccion', 2, 2, 2), 
+      (3, 'servicio de asesoria legal', 5, 5, 3), 
+      (4, 'servicio de marketing', 1, 1, 4), 
+      (5, 'servicio de redaccion', 2, 2, 5)
+    `
+  );
+
+  await connection.query(
+    `
+      INSERT INTO COMENTARIOS(id, id_usuarios, id_servicios, texto) VALUES
+      (1, 1, 3, 'comentario de servicio de promagracion'), 
+      (2, 2, 2, 'comentario de servicio de traduccion'), 
+      (3, 3, 5, 'comentario de servicio de asesoria legal'), 
+      (4, 4, 1, 'comentario de servicio de marketing'), 
+      (5, 5, 2, 'comentario de servicio de redaccion')
+    `
+  );
+
 } catch (error) {
   console.error(error);
 } finally {
